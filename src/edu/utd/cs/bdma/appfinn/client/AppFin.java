6402add622f5b2d418ad540173846a4495ad8d59
Varun S Hariharan <varunshariharan@gmail.com>
2016-07-21 17:12:24 -0500

    New Project

--- src/edu/utd/cs/bdma/appfinn/client/AppFin.java
+++ src/edu/utd/cs/bdma/appfinn/client/AppFin.java
@@ -27,7 +27,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 public class AppFin implements EntryPoint {
 
-//	private String link = "http://127.0.0.1:8888/appfin/";
+/////	private String link = "http://127.0.0.1:8888/appfin/";
 	private String link = "http://127.0.0.1:8080/appfin/";
 	private String dataDir = "/data/kld/android_apps_project/app_fingerprinting/";
 	String [] folderNames = new String[]{"d"};
